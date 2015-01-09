e95e9a7a2542e98fbdc324df8f7f042feafba5e3
Louise Dennis <louise@dennis-sellers.com>
2015-01-09 17:05:10 +0000

    Starting work to tidy and comment the implementation.

--- src/examples/eass/sticky_wheel/SimpleWheeledRobotEnv.java
+++ src/examples/eass/sticky_wheel/SimpleWheeledRobotEnv.java
@@ -1,8 +1,29 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) 2015 Louise A. Dennis, and Michael Fisher 
+// 
+// This file is part of the Engineering Autonomous Space Software (EASS) Library.
+// 
+// The EASS Library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+// 
+// The EASS Library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with the EASS Library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+// To contact the authors:
+// http://www.csc.liv.ac.uk/~lad
+//
+//----------------------------------------------------------------------------
 package eass.sticky_wheel;
 
-import gwendolen.uavs.prism.SimpleUAV;
 import ail.mas.ActionScheduler;
-import ail.mas.vehicle.VehicleEnv;
 import eass.mas.vehicle.EASSVehicleEnvironment;
 import eass.semantics.EASSAgent;
 import ail.semantics.AILAgent;
@@ -14,6 +35,18 @@ import ail.syntax.NumberTerm;
 import ail.util.AILexception;
 import ajpf.MCAPLJobber;
 
+import javax.swing.JPanel;
+
+/**
+ * SimpleWheeledRobot Env is an environment for a simple wheeled robot.  This is using the EASS classes for
+ * Vehicles, so the environment contain vehicles which in turn act as environments for sets of agents which control that
+ * individual vehicle.  
+ * 
+ * This environment determines what happens when a wheeled robot moves (i.e., engages both its motors).  It contains no obstacles 
+ * or anything else but it does allow one of a robot's wheels to become sticky and calculates the effect this has on movement.
+ * @author louiseadennis
+ *
+ */
 public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCAPLJobber {
 	String name = "Simple Wheeled Robot Env";
 	ActionScheduler scheduler;
@@ -27,6 +60,9 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 	
 	WheeledRobotUI gui;
 	
+	/**
+	 * Standard constructor formulation for an EASS Environment.
+	 */
 	public SimpleWheeledRobotEnv() {
 		super();
 		ActionScheduler s = new ActionScheduler();
@@ -37,9 +73,15 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 	}
 	
 
+	/*
+	 * (non-Javadoc)
+	 * @see ail.mas.DefaultEnvironment#addAgent(ail.semantics.AILAgent)
+	 */
 	public void addAgent(AILAgent a) {
+		// NB. We are implicitly assuming that abstraction engines will be added before reasoning engines.
 		EASSAgent eass = (EASSAgent) a;
 		
+		// If the agent is an reasoning engine, connect it to its Vehicle.
 		if (contains(eass.getReasoningName())) {
 			this.addAgentToVehicle(eass);
 			Predicate position = new Predicate("position");
@@ -48,6 +90,7 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 			position.addTerm(new NumberTermImpl(theta));
 			getVehicle(eass.getReasoningName()).addPercept(position);
 		} else {
+			// Else create a TwoWheeledRobot object to act as the environment for the agent.
 			TwoWheeledRobot r = new TwoWheeledRobot(a);
 			addVehicle(r);
 			rName = r.getName();
@@ -67,10 +110,11 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 			double iterations  = ((NumberTerm) act.getTerm(0)).solve();
 			double motorpower1 = r.getMotor1Power();
 			double motorp2 = r.getMotor2Power();
+			// Motor 2's actual power depends upon the stickiness of the wheel.
 			double motorpower2 = motorp2 - (motorp2*sticky_modifier);
 			
-			
-			// For the  time being we assume all constants are 1 so motorpower is directly equivalent to the forward velocity of each wheel
+			// For the  time being we assume all constants (for wheel size etc.) are 1 
+			// so motorpower is directly equivalent to the forward velocity of each wheel.
 			
 			int count = 0;
 			while (count < iterations) {
@@ -78,12 +122,15 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 				double xdot = Math.cos(Math.toRadians(theta)) * robot_velocity;
 				double ydot = Math.sin(Math.toRadians(theta)) * robot_velocity;
 				double thetadot = (motorpower1 - motorpower2)/2;
-					
 			
 				// We assume 1 time step has elapsed
 				x += xdot;
 				y += ydot;
 				theta += thetadot;
+				
+				// NB. The above equations are standard but don't always accurately give the movement
+				// e.g., if one wheel isn't turning at all.
+				
 				Predicate position = new Predicate("position");
 				position.addTerm(new NumberTermImpl(x));
 				position.addTerm(new NumberTermImpl(y));
@@ -91,28 +138,45 @@ public class SimpleWheeledRobotEnv extends EASSVehicleEnvironment implements MCA
 			 
 				getVehicle(agName).addPercept(position);
 
-				((WheeledRobotUI) gui).updateGraphics(x, y, theta);
+				// Vehicle environments may have GUI interfaces.  We assume that we have a WheeledRobotUI interface for this and
+				// update it with the robot's new position.
+				((JPanel) gui).updateGraphics(x, y, theta);
 				count++;
 			}
-			System.err.println("(" + x + " ," + y + ")");
-
 		} 
 		
 		return super.executeAction(agName, act);
 	}
 	
+	/**
+	 * Return the x position of the vehicle.
+	 * @return
+	 */
 	public double getX() {
 		return x;
 	}
 	
+	/**
+	 * Return the y position of the vehicle.
+	 * @return
+	 */
 	public double getY() {
 		return y;
 	}
 	
+	/**
+	 * Return the orientation of the vehicle.
+	 * @return
+	 */
 	public double getTheta() {
 		return theta;
 	}
 	
+	/**
+	 * Set the target location for the robot - presumably coming in from an external control system.
+	 * @param rname
+	 * @param p
+	 */
 	public void setTarget(String rname, Predicate p) {
 		addPercept(rname, p);
 	}
