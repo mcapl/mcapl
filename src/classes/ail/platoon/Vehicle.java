8b95b62d8e81f9c56c87a56c2e61dd94d673b50b
maryam-k <maryamkamali@yahoo.com>
2015-02-19 18:14:16 +0000

    input and output data streams

--- src/classes/ail/platoon/Vehicle.java
+++ src/classes/ail/platoon/Vehicle.java
@@ -2,8 +2,10 @@ package ail.platoon;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 import java.util.Random;
 
+import ail.syntax.Unifier;
 import ail.util.AILSocketServer;
 
 public class Vehicle {
@@ -19,7 +21,19 @@ public class Vehicle {
 	public VehicleData ego = new VehicleData();
 	public VehicleData leader = new VehicleData();
 	public VehicleData preceding = new VehicleData();
-	
+
+	public String[] command_list = {"", 
+									"send_message_too_close", 
+									"send_message_too_far",
+									"breaking_platoon",
+									"set_distance_parameter_for_latency(0)",
+									"set_distance_parameter_for_latency(1)",
+									"set_distance_parameter_for_latency(2)",
+									"set_distance_parameter_for_latency(3)",
+									"request_to_join",
+									"assign_platoon_id",
+									"switch_cruise_to_platoon",
+									"switch_platoon_to_cruise"};
 	/* Set up our sockets */
 	public Vehicle(int id) {
 		ego.uniqueID = id;
@@ -35,9 +49,27 @@ public class Vehicle {
 		outbound = new AILSocketServer(BASE_OUTBOUND_PORT + id);
 		System.out.println("Connected!");
 	}
+
+	public void execute(String command) {
+		int command_number = 0;
+		
+		for(int i=0; i< command_list.length; i++){
+			if(command.equals(command_list[i]))
+				command_number= i;
+		}
+		
+		/* Create buffer to transmit our command */
+		ByteBuffer buf2 = ByteBuffer.allocate(4);
+		buf2.order(ByteOrder.LITTLE_ENDIAN);
+		buf2.putInt(command_number);
+		
+		/* Write to outbound socket */
+		outbound.write(buf2);
+//		System.out.println("\n command_number is "+ command_number+ " and it is "+ command);		
+	}
 	
 	/* Update our data and send a command */
-	public boolean update(int command) {
+	public boolean update() {
 		
 		/* Set up a receive buffer */
 		ByteBuffer buf = ByteBuffer.allocate(33*8);
@@ -91,52 +123,72 @@ public class Vehicle {
 		preceding.azimuth = buf.getDouble();
 		preceding.lateralPosition = buf.getDouble();
 		
-		/* Create buffer to transmit our command */
-		ByteBuffer buf2 = ByteBuffer.allocate(4);
-		buf2.order(ByteOrder.LITTLE_ENDIAN);
-		buf2.putInt(command);
-		
-		/* Write to outbound socket */
-		outbound.write(buf2);
 		return true;
 	}
 	
-	/* Quick example usage */
-	public static void main(String [] args) {
-		/*
-		 * Vehicle instantiation must be in the same order that the vehicles are initiated in Simulink.
-		 * This is because the service.accept() call blocks until the relevent Simulink block is connected
-		 */
-		Vehicle v = new Vehicle(3);
-//		Vehicle v2 = new Vehicle(5);
-		
-		/* Loop forever and update from Simulink, read will block so the processes naturally sync */
-		while (true) {
-			
-			/*
-			 * Call update with a random integer (as we have no commands to send yet!)
-			 * These calls also need to be in the order than Simulink executes the blocks
-			 * (check sorted executed order)
-			 */
-			if (v.update((new Random()).nextInt(10))) { //) && v2.update((new Random()).nextInt(10))) {
-				/* Print out some information */
-				System.out.print(String.format("%.1f",v.ego.timestamp) + 
-						"s: Vehicle " + v.ego.uniqueID + " is travelling at " +
-						String.format("%.1f", v.ego.speed) + "m/s and is ");
-				if (v.ego.range < 100) {
-					System.out.print(String.format("%.2f", v.ego.range) + "m");
-				} else {
-					System.out.print("not");
-				}
-				System.out.println(" behind another vehicle");
-/*				System.out.println(String.format("%.1f",v2.ego.timestamp) + 
-						"s: Vehicle " + v2.ego.uniqueID + " is travelling at " +
-						String.format("%.1f", v2.ego.speed) + "m/s and is " +
-						String.format("%.2f", v2.ego.range) + "m behind another vehicle");*/
-			} else{
-				return;
-			}
-		}
+	public double getDistance(){
+		return ego.range;
 	}
-		
+
+	public int getID(){
+		return ego.uniqueID;
+	}
+	
+	public int getprecedingPID(){
+		return preceding.platoonID;
+	}
+	
+	public int getegoPID(){
+		return ego.uniqueID;
+	}
+	
+	public double getprecedingSpeed(){
+		return ego.speed;
+	}
+	
+	public double getSpeed(){
+		return preceding.speed;
+	}
+	
+	
+//	
+//	/* Quick example usage */
+//	public static void main(String [] args) {
+//		/*
+//		 * Vehicle instantiation must be in the same order that the vehicles are initiated in Simulink.
+//		 * This is because the service.accept() call blocks until the relevent Simulink block is connected
+//		 */
+//		Vehicle v = new Vehicle(3);
+////		Vehicle v2 = new Vehicle(5);
+//		
+//		/* Loop forever and update from Simulink, read will block so the processes naturally sync */
+//		while (true) {
+//			
+//			/*
+//			 * Call update with a random integer (as we have no commands to send yet!)
+//			 * These calls also need to be in the order than Simulink executes the blocks
+//			 * (check sorted executed order)
+//			 */
+//			if (v.update((new Random()).nextInt(10))) { //) && v2.update((new Random()).nextInt(10))) {
+//				/* Print out some information */
+//				System.out.print(String.format("%.1f",v.ego.timestamp) + 
+//						"s: Vehicle " + v.ego.uniqueID + " is travelling at " +
+//						String.format("%.1f", v.ego.speed) + "m/s and is ");
+//				if (v.ego.range < 100) {
+//					System.out.print(String.format("%.2f", v.ego.range) + "m");
+//				} else {
+//					System.out.print("not");
+//				}
+//				System.out.println(" behind another vehicle");
+///*				System.out.println(String.format("%.1f",v2.ego.timestamp) + 
+//						"s: Vehicle " + v2.ego.uniqueID + " is travelling at " +
+//						String.format("%.1f", v2.ego.speed) + "m/s and is " +
+//						String.format("%.2f", v2.ego.range) + "m behind another vehicle");*/
+//			} else{
+//				return;
+//			}
+//		}
+//	}
+
+	
 }
