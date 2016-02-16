f152290dcdeb59f4aa75a38aa3ba3b0bf564587c
Louise Dennis <louise@dennis-sellers.com>
2016-02-16 14:52:57 +0000

    Removing unused imports.

--- src/classes/ail/syntax/Equation.java
+++ src/classes/ail/syntax/Equation.java
@@ -30,8 +30,6 @@ package ail.syntax;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
 
 import ail.semantics.AILAgent;
 
