f1fae331bcaa344e491eacb29fb648ac3a1249aa
peterstringer <peterstringer@localhost>
2020-08-15 16:27:42 +0100

    Added JUnit test file for Goal State Handling

---
+++ src/tests/ail/semantics/GoalStateHandlingQuickTests.java
@@ -0,0 +1,23 @@
+package ail.semantics;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import ail.syntax.Literal;
+import ail.syntax.VarTerm;
+import ail.util.AILPrettyPrinter;
+import ail.syntax.GBelief;
+import ail.syntax.Unifier;
+import ail.syntax.Guard;
+import ail.syntax.Intention;
+import ail.syntax.Goal;
+import ail.syntax.Event;
+import ail.semantics.operationalrules.HandleAddAchieveTestGoalwEvent;
+import ail.semantics.operationalrules.HandleDropGeneralGoal;
+import ail.syntax.Deed;
+
+import java.util.LinkedList;
+
+public class GoalStateHandlingQuickTests {
+
+}
