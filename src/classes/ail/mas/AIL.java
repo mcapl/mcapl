fee758531db10c114fc4f35ede745036a3340099
t95229ld <louise.dennis@manchester.ac.uk>
2020-12-07 15:10:48 +0000

    Updating print out to state this is development version.

--- src/classes/ail/mas/AIL.java
+++ src/classes/ail/mas/AIL.java
@@ -45,7 +45,7 @@ public class AIL {
 	@FilterField
 	static String logname = "ail.mas.AIL";
 	
-	static String version = "MCAPL Framework 2020";
+	static String version = "MCAPL Development Version 2020";
 
 	/**
 	 * Main method.  There should be one argument consisting of the name of a configuration file.
