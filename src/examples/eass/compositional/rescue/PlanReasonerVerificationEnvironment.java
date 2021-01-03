c3a90f28647fb02ec060a2b84bd4e628bc0a4d6e
t95229ld <louise.dennis@manchester.ac.uk>
2021-01-03 12:40:39 +0000

    Working version.
    Updated to allow battery calculations.

--- src/examples/eass/compositional/rescue/PlanReasonerVerificationEnvironment.java
+++ src/examples/eass/compositional/rescue/PlanReasonerVerificationEnvironment.java
@@ -6,8 +6,13 @@ import java.util.TreeSet;
 import ail.syntax.Action;
 import ail.syntax.Literal;
 import ail.syntax.Message;
+import ail.syntax.NumberTerm;
 import ail.syntax.NumberTermImpl;
 import ail.syntax.Predicate;
+import ail.syntax.Unifier;
+import ail.syntax.VarTerm;
+import ail.util.AILexception;
+import ajpf.util.AJPFLogger;
 import eass.mas.verification.EASSVerificationEnvironment;
 import eass.semantics.EASSAgent;
 
@@ -21,12 +26,15 @@ public class PlanReasonerVerificationEnvironment extends EASSVerificationEnviron
 		TreeSet<Predicate> percepts = new TreeSet<Predicate>();
 		boolean need_recharge = random_bool_generator.nextBoolean();
 		
+		Literal battery = new Literal("battery");
 		if (need_recharge) {
-			System.out.println("battery Low");
-			percepts.add(new Literal("battery_low"));
+			System.out.println("battery(5)");
+			battery.addTerm(new NumberTermImpl(24));
 		} else {
 			System.out.println("Battery fine");
+			battery.addTerm(new NumberTermImpl(50));
 		}
+		percepts.add(battery);
 		
 		boolean target = random_bool_generator.nextBoolean();
 		
@@ -79,5 +87,25 @@ public class PlanReasonerVerificationEnvironment extends EASSVerificationEnviron
 
 		return messages;
 	}
+	
+	public Unifier executeAction(String agName, Action act) throws AILexception {
+
+	   	// We exclude remove_shared and assert_shared assuming these are instantaneous.
+	   	if (!act.getFunctor().equals("minus")) {
+	   		return super.executeAction(agName, act);
+	   	} else {
+	   		Unifier u = new Unifier();
+	   		NumberTerm x = (NumberTerm) act.getTerm(0);
+    		NumberTerm y = (NumberTerm) act.getTerm(1);
+    		VarTerm sum = (VarTerm) act.getTerm(2);
+    		double d = x.solve() - y.solve();
+    		NumberTermImpl z = new NumberTermImpl(d);
+    		u.unifies(sum, z);
+    		return u;
+	   	}
+	   	
+	   	// return new Unifier();
+	}
+
 
 }
