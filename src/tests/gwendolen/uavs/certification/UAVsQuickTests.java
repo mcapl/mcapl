deb30eb51cbe12f69734bcd32a833959365d3690
t95229ld <louise.dennis@manchester.ac.uk>
2021-08-26 15:10:15 +0100

    Tweaks?

--- src/tests/gwendolen/uavs/certification/UAVsQuickTests.java
+++ src/tests/gwendolen/uavs/certification/UAVsQuickTests.java
@@ -25,15 +25,30 @@ import ajpf.MCAPLcontroller;
 public class UAVsQuickTests extends TestJPF {
 	 static final String[] JPF_ARGS = { "suppress_version = true" };
 	
+
 	  @Test //----------------------------------------------------------------------
-	  public void certification_ajpf () {
+	  public void certification_ajpf_taxi () {
 	   if (verifyNoPropertyViolation(JPF_ARGS)) {
 	    	String filename =  "/src/examples/gwendolen/uavs/certification/uav1.ail";
 	    	String prop_filename =  "/src/examples/gwendolen/uavs/certification/certification.psl";
 	    	String[] args = new String[3];
 	    	args[0] = filename;
 	    	args[1] = prop_filename;
-	    	args[2] = "1";
+	    	args[2] = "2";
+	    	AJPF_w_AIL.run(args);
+	   }
+	 	// }
+	  }
+
+	  @Test //----------------------------------------------------------------------
+	  public void certification_ajpf_lineup () {
+	   if (verifyNoPropertyViolation(JPF_ARGS)) {
+	    	String filename =  "/src/examples/gwendolen/uavs/certification/uav1.ail";
+	    	String prop_filename =  "/src/examples/gwendolen/uavs/certification/certification.psl";
+	    	String[] args = new String[3];
+	    	args[0] = filename;
+	    	args[1] = prop_filename;
+	    	args[2] = "3";
 	    	AJPF_w_AIL.run(args);
 	   }
 	 	// }
