173d711a5193e423bd6034a7b247f8785ffdaf8e
t95229ld <louise.dennis@manchester.ac.uk>
2021-12-01 13:11:09 +0000

    Adding back development version message.

--- src/classes/ail/mas/AIL.java
+++ src/classes/ail/mas/AIL.java
@@ -45,7 +45,7 @@ public class AIL {
 	@FilterField
 	static String logname = "ail.mas.AIL";
 	
-	static String version = "MCAPL Framework 2021";
+	static String version = "MCAPL Framework Development Version 2021";
 
 	/**
 	 * Main method.  There should be one argument consisting of the name of a configuration file.
