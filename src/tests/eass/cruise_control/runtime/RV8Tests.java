e18b6b4cb3879a96cfeaaf3f937fefa9a47223d3
t95229ld <louise.dennis@manchester.ac.uk>
2024-07-13 17:19:17 +0100

    Final test.

--- src/tests/eass/cruise_control/runtime/RV8Tests.java
+++ src/tests/eass/cruise_control/runtime/RV8Tests.java
@@ -23,130 +23,17 @@
 //----------------------------------------------------------------------------
 package eass.cruise_control.runtime;
 
-import org.junit.Test;
-
 import ail.util.AJPF_w_AIL;
 import gov.nasa.jpf.util.TypeRef;
 import gov.nasa.jpf.util.test.TestJPF;
+import org.junit.Test;
 
-public class RVTests extends TestJPF {
+public class RV8Tests extends TestJPF {
 	 static final String[] JPF_ARGS = {};
 
 	 //--- driver to execute single test methods
 	  public static void main(String[] args) {
 		  runTestsOfThisClass(args);
-	  }
-
-	  @Test //----------------------------------------------------------------------
-	  public void TraceAsUnstructuredTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/trace_as_unstructured.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void BrakeOrAcclerateTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/brake_or_accelerate_verification.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void StandardSafeTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/standard_verification_safe.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void SafeOrAccelerateTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/safe_or_accelerate_verification.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void SafeOrAccelerateUnsafeTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/safe_or_accelerate_verification_unsafe.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void TwoConstraintsTest() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/two_constraints_verification_safe.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
-	  }
-	  
-	  @Test //----------------------------------------------------------------------
-	  public void TwoConstraintsTestUnsafe() {
-		  if (verifyNoPropertyViolation(JPF_ARGS)) {
-		    	String filename =  "src/examples/eass/cruise_control/runtime/two_constraints_verification_unsafe.ail";
-		    	String prop_filename =  "src/examples/eass/cruise_control/motorway.psl";
-		    	String[] args = new String[3];
-		    	args[0] = filename;
-		    	args[1] = prop_filename;
-		    	args[2] = "5";
-		    	AJPF_w_AIL.run(args);
-		  } else {
-			    	 
-		  }
-
 	  }
 
 	  @Test //----------------------------------------------------------------------
@@ -160,7 +47,7 @@ public class RVTests extends TestJPF {
 		    	args[2] = "5";
 		    	AJPF_w_AIL.run(args);
 		  } else {
-		    	 
+
 		  }
 	  }
 
